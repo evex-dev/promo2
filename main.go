2981237d2c3a2da0b0993806c1171a97d55974b1
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-09-07 23:23:58 +0900

    uoload

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+	"github.com/evex-dev/promo2/src/connect"
+)
+
+func main() {
+	validatePath := func(input string) error {
+		if _, err := os.Stat(input); err != nil {
+			return err
+		}
+		
+		return nil
+	}
+
+	proxiesPathPrompt := promptui.Prompt{
+		Label:    "Proxies Path",
+		Validate: validatePath,
+	}
+
+	proxiesPath, err := proxiesPathPrompt.Run()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	proxiesBody, err := os.Open(proxiesPath)
+	
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	scanner := bufio.NewScanner(proxiesBody)
+	var proxylist []string
+	for scanner.Scan() {
+		proxylist = append(proxylist, scanner.Text())
+	}
+
+	resultPathPrompt := promptui.Prompt{
+		Label:    "Result Path",
+		Validate: validatePath,
+	}
+
+	resultPath, err := resultPathPrompt.Run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		fmt.Println("\x1b[33m[=] Requesting...\x1b[0m")
+		
+		proxyUrl := ""
+
+		if len(proxylist) > 0 {
+			proxyUrl = proxylist[rand.Intn(len(proxylist))]
+		}
+
+		promos, err := connect.getPromoUrls(proxyUrl)
+
+		if err != nil {
+			fmt.Println("\x1b[31m[-] Error: ", err, "\x1b[0m")
+			continue
+		}
+
+		resultContent := strings.Join(promos, "\n")
+		resultFile, err := os.Open(resultPath)
+
+		if err != nil {
+			fmt.Println("\x1b[31m[-] Error: ", err, "\x1b[0m")
+			continue
+		}
+
+		scanner := bufio.NewScanner(resultFile)
+		var resultlist []string
+		for scanner.Scan() {
+			resultlist = append(resultlist, scanner.Text())
+		}
+
+		resultFile.WriteString(fmt.Sprintf("%s\n%s", strings.Join(resultlist, "\n"), resultContent))
+
+		for _, promo := range promos {
+			fmt.Println("\x1b[32m[+] Found: ", promo, "\x1b[0m")
+		}
+	}
+}
---
+++ src/connect.go
@@ -0,0 +1,134 @@
+package connect
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type RequestParms struct {
+	UserUUID string `json:"userUuid"`
+	CampaignID string `json:"campaignId"`
+}
+
+
+type Games []struct {
+	Players []Player `json:"players"`
+}
+
+type Player struct {
+	UUID string `json:"uuid"`
+}
+
+func getPromoUrls(proxyUrl string) ([]string, error) {
+	prxoyURL, err := url.Parse(proxyUrl)
+
+	if err != nil {
+		return nil, fmt.Errorf("invalid proxy URL: %s", err)
+	}
+
+	transport := http.DefaultTransport
+
+	if proxyUrl != "" {
+		transport = &http.Transport{
+			Proxy: http.ProxyURL(prxoyURL),
+		}
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+		Transport:  transport,
+	}
+
+	setupReq, err := http.NewRequest("GET", "https://www.chess.com/service/gamelist/top?limit=50&from=" + generateRandomIntString(1000), nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect setupReqURL")
+	}
+	resp, err := client.Do(setupReq)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect setupReqURL")
+	}
+	defer resp.Body.Close()
+
+	payload, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response")
+	}
+
+	var games Games
+
+	err = json.Unmarshal(payload, &games)
+	if err != nil {
+		return nil, fmt.Errorf("games unmarshal error")
+	}
+
+	promoUrls := []string{}
+
+	for _, game := range games {
+		for _, user := range game.Players {
+			body, err := json.Marshal(RequestParms{
+				UserUUID: user.UUID,
+				CampaignID: "4daf403e-66eb-11ef-96ab-ad0a069940ce",
+			})
+			if err != nil {
+				return nil, fmt.Errorf("Unknown Error")
+			}
+		
+			req, err := http.NewRequest("POST", "https://www.chess.com/rpc/chesscom.partnership_offer_codes.v1.PartnershipOfferCodesService/RetrieveOfferCode", bytes.NewBuffer(body))
+			if err != nil {
+				return nil, fmt.Errorf("Unknown Error")
+			}
+			
+			headers := http.Header{}
+			headers.Add("accept", "application/json, text/plain, */*")
+			headers.Add("accept-language", "ja,en-US;q=0.9,en;q=0.8")
+			headers.Add("cache-control", "no-cache")
+			headers.Add("content-type", "application/json")
+			headers.Add("pragma", "no-cache")
+			headers.Add("priority", "u=1, i")
+			headers.Add("sec-ch-ua", "\"Chromium\";v=\"" + generateRandomIntString(200) + "\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"")
+			headers.Add("sec-ch-ua-mobile", "?0")
+			headers.Add("sec-ch-ua-platform", "\"Windows\"")
+			headers.Add("sec-fetch-dest", "empty")
+			headers.Add("sec-fetch-mode", "cors")
+			headers.Add("sec-fetch-site", "same-origin")
+			headers.Add("referrer", "https://www.chess.com/play/computer/discord-wumpus?utm_source=chesscom&utm_medium=homepagebanner&utm_campaign=discord2024")
+			req.Header = headers
+			
+			resp, err = client.Do(req)
+			if err != nil {
+				return nil, fmt.Errorf("Connection Error")
+			}
+
+			if resp.Status == "200" {
+				defer resp.Body.Close()
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return nil, fmt.Errorf("")
+				}
+				var data map[string]interface{}
+				err = json.Unmarshal(body,&data)
+				if err != nil {
+					return nil, fmt.Errorf("Unknown Error")
+				}
+				if data["codeValue"] != nil {
+					content_data := "https://discord.com/billing/promotions/" + data["codeValue"].(string)
+
+					promoUrls = append(promoUrls, content_data)
+				}
+			}
+		}
+	}
+
+	return promoUrls, nil
+}
+
+func generateRandomIntString(max int) string {
+	// 0 ~ 1000
+	return string(rand.Intn(max))
+}
