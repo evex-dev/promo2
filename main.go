72ec9ebbbfe00e71ec7db87f8e446f52cdb545fa
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-09-08 00:12:35 +0900

    update

--- main.go
+++ main.go
@@ -6,17 +6,20 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"sync"
 
-	"github.com/manifoldco/promptui"
 	"github.com/evex-dev/promo2/src/connect"
+	"github.com/manifoldco/promptui"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	validatePath := func(input string) error {
 		if _, err := os.Stat(input); err != nil {
 			return err
 		}
-		
+
 		return nil
 	}
 
@@ -33,12 +36,12 @@ func main() {
 	}
 
 	proxiesBody, err := os.Open(proxiesPath)
-	
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	
+
 	scanner := bufio.NewScanner(proxiesBody)
 	var proxylist []string
 	for scanner.Scan() {
@@ -57,41 +60,58 @@ func main() {
 	}
 
 	for {
-		fmt.Println("\x1b[33m[=] Requesting...\x1b[0m")
-		
-		proxyUrl := ""
+		oneGenerate := func() {
+			wg.Add(1)
+			fmt.Println("\x1b[33m[=] Requesting...\x1b[0m")
 
-		if len(proxylist) > 0 {
-			proxyUrl = proxylist[rand.Intn(len(proxylist))]
+			proxyUrl := ""
+
+			if len(proxylist) > 0 {
+				proxyUrl = proxylist[rand.Intn(len(proxylist))]
+			}
+
+			fmt.Println("\x1b[35m[~] Proxy: ", proxyUrl, "\x1b[0m")
+
+			promos, err := connect.GetPromoUrls(proxyUrl)
+
+			if err != nil {
+				fmt.Println("\x1b[31m[-] Error: ", err, "\x1b[0m")
+				wg.Done()
+				return
+			}
+
+			fmt.Println("\x1b[32m[+] Found: ", len(promos), "urls\x1b[0m")
+
+			resultContent := strings.Join(promos, "\n")
+			resultFile, err := os.Open(resultPath)
+
+			if err != nil {
+				fmt.Println("\x1b[31m[-] Error: ", err, "\x1b[0m")
+				wg.Done()
+				return
+			}
+
+			scanner := bufio.NewScanner(resultFile)
+			var resultlist []string
+			for scanner.Scan() {
+				resultlist = append(resultlist, scanner.Text())
+			}
+
+			resultFile.WriteString(fmt.Sprintf("%s\n%s", strings.Join(resultlist, "\n"), resultContent))
+
+			for _, promo := range promos {
+				fmt.Println("\x1b[32m[+] Found: ", promo, "\x1b[0m")
+			}
+
+			wg.Done()
 		}
 
-		promos, err := connect.GetPromoUrls(proxyUrl)
+		go oneGenerate()
+		go oneGenerate()
+		go oneGenerate()
+		go oneGenerate()
+		go oneGenerate()
 
-		if err != nil {
-			fmt.Println("\x1b[31m[-] Error: ", err, "\x1b[0m")
-			continue
-		}
-
-		fmt.Println("\x1b[32m[+] Found: ", len(promos), "urls\x1b[0m")
-
-		resultContent := strings.Join(promos, "\n")
-		resultFile, err := os.Open(resultPath)
-
-		if err != nil {
-			fmt.Println("\x1b[31m[-] Error: ", err, "\x1b[0m")
-			continue
-		}
-
-		scanner := bufio.NewScanner(resultFile)
-		var resultlist []string
-		for scanner.Scan() {
-			resultlist = append(resultlist, scanner.Text())
-		}
-
-		resultFile.WriteString(fmt.Sprintf("%s\n%s", strings.Join(resultlist, "\n"), resultContent))
-
-		for _, promo := range promos {
-			fmt.Println("\x1b[32m[+] Found: ", promo, "\x1b[0m")
-		}
+		wg.Wait()
 	}
 }
